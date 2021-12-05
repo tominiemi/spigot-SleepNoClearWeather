6fdd9954f6aa6d16ba3bdf7acf0b61943d6b8b15
tominiemi <btnnewsguy@gmail.com>
2021-12-06 00:14:52 +0200

    Implementing first bed event things

--- src/com/tomi/sleepnoclearweather/SleepListener.java
+++ src/com/tomi/sleepnoclearweather/SleepListener.java
@@ -1,16 +1,37 @@
 package com.tomi.sleepnoclearweather;
 
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.*;
 
 public class SleepListener implements Listener {
 	
+	Player player;
+	World world;
+	
+	int stormDuration = 0;
+	
 	@EventHandler
-	public static void onPlayerBedEnter(PlayerBedEnterEvent event) {
-		//do something
-		event.getPlayer().sendMessage("you have entered a bed");
+	public void onPlayerBedEnter(PlayerBedEnterEvent event) {
+		//if sleep is not ok then return
+		if (event.getBedEnterResult() != PlayerBedEnterEvent.BedEnterResult.OK) return;
+		
+		player = event.getPlayer();
+		player.sendMessage("you have entered a bed"); //debug message
+		world = player.getWorld();
+		
+		//if raining or snowing, get how long it's going to rain or snow
+		if (world.hasStorm()) stormDuration = world.getWeatherDuration();
+		player.sendMessage("amount of weathertime remaining: " + stormDuration); //debug message
+		
 	}
 	
+	@EventHandler
+	public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
+		player = event.getPlayer();
+		player.sendMessage("you have entered a bed"); //debug message
+	}
 	
 }
