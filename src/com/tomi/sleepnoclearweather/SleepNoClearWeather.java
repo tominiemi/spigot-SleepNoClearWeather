ffbd088fc0f7e3a7a9c0277f9232d3a8d00367ae
tominiemi <btnnewsguy@gmail.com>
2021-12-05 23:11:10 +0200

    First Blank Plugin

---
+++ src/com/tomi/sleepnoclearweather/SleepNoClearWeather.java
@@ -0,0 +1,23 @@
+package com.tomi.sleepnoclearweather;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author tomi
+ * Makes it so when you sleep it doesn't clear the weather (rain, snow etc.)
+ */
+public class SleepNoClearWeather extends JavaPlugin {
+	
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() { 
+    	
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() { 
+    	
+    }
+	
+	
+}
